b30081f78a103fe25085bf3f4158c49dc8716e16
robbecallewaert <robbecallewaert@MacBook-Pro-van-Robbe-2.local>
2017-12-13 12:04:31 +0100

    heroku production build

---
+++ dist/server.js
@@ -0,0 +1,31 @@
+const Hapi = require(`hapi`);
+const server = new Hapi.Server();
+server.connection({
+  port: process.env.PORT || 8080,
+  host: `0.0.0.0`
+});
+
+const inert = require(`inert`);
+server.register(inert, err => {
+  if (err) {
+    throw err;
+  }
+  server.route({
+    method: `GET`,
+    path: `/{param*}`,
+    handler: {
+      directory: {
+        path: `./public`,
+        redirectToSlash: true,
+        index: true
+      }
+    }
+  });
+});
+
+server.start(err => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
+});
